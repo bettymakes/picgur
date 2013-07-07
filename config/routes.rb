787d673545f99e8ad472e59abe52e754ee684aa9
blibotics <blibotics@gmail.com>
2013-07-07 17:33:39 -0400

    Adding create and new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Picgur::Application.routes.draw do
+  post 'pictures' => 'pictures#create'
   get 'pictures' => 'pictures#index'
-
+  get 'pictures/new' => 'pictures#new'
   get 'pictures/:id' => 'pictures#show'
 end
