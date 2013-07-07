52e74cc026579f19ecff49a9fa4edc73ab8dddbe
blibotics <blibotics@gmail.com>
2013-07-07 19:21:34 -0400

    Creating database, cleaning up controller file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Picture.create!(
+  :title  => 'Penguin',
+  :artist => 'pingu',
+  :url    => "http://upload.wikimedia.org/wikipedia/en/c/c2/Pingu.png"
+)
+
+Picture.create!(
+  :title  => 'Baby Pandas',
+  :artist => 'Panda Lover',
+  :url    => "http://pandastephanienguyen.edublogs.org/files/2010/08/baby-panda2.jpg"
+)
+
+Picture.create!(
+  :title  => 'Baby Pig',
+  :artist => 'piggytime',
+  :url    => "http://www.crinkleblog.com/wp-content/uploads/2013/04/adorable-baby-piglet.jpg"
+)
