86782ebade2c5e70a5816a786cc55939c5fe485b
blibotics <blibotics@gmail.com>
2013-07-07 16:38:10 -0400

    adding individual picture routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Picgur::Application.routes.draw do
   get 'pictures' => 'pictures#index'
+
+  get 'pictures/0' => 'pictures#picture0'
+  get 'pictures/1' => 'pictures#picture1'
+  get 'pictures/2' => 'pictures#picture2'
 end
