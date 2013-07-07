66a7fb8be5b5debf5f97f512c161187af903cf2f
blibotics <blibotics@gmail.com>
2013-07-07 15:44:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Picgur::Application.config.secret_token = '48d3b6b923988434361602c768e269fe6dd918454f4da2ca27a849fbb977097d787f74a47cafce3e9da4ab8a3b70d2b3c5fe6b05d9b01cf6bbe93ae95b741187'
