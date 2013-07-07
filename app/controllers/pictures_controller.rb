272da3275d25a5d2aae4fec05bca6f6af623cbde
blibotics <blibotics@gmail.com>
2013-07-07 16:28:02 -0400

    adding pictures array

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,26 @@
 class PicturesController < ActionController::Base
 
   def index
+    @pictures = [
+      {
+        :title  => 'Penguin',
+        :artist => 'pingu',
+        :url    => "http://upload.wikimedia.org/wikipedia/en/c/c2/Pingu.png"
+      },
+
+      {
+        :title  => 'Baby Pandas',
+        :artist => 'Panda Lover',
+        :url    => "http://pandastephanienguyen.edublogs.org/files/2010/08/baby-panda2.jpg"
+      },
+
+      {
+        :title  => 'Baby Pig',
+        :artist => 'piggytime',
+        :url    => "http://www.crinkleblog.com/wp-content/uploads/2013/04/adorable-baby-piglet.jpg"
+      }
+    ]
+
   end
 
 end
