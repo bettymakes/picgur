bb455909edbad62fa342a6125dca700f601b73c2
blibotics <blibotics@gmail.com>
2013-07-07 20:20:29 -0400

    Reassigning root route to pictures#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Picgur::Application.routes.draw do
+  root :to => 'pictures#index'
   post 'pictures' => 'pictures#create'
   get 'pictures' => 'pictures#index'
   get 'pictures/new' => 'pictures#new'
